aa03a7c1295ee38dedf45b6685161e871a59f34a
xhan469 <xhan469@aucklanduni.ac.nz>
2024-09-24 03:09:59 +1200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import HeroSection from './components/HeroSection'
+import FeatureSection from './components/FeatureSection'
+import WorkFlow from './components/WorkFlow'
+import Pricing from './components/Pricing'
+import Testimonials from './components/Testimonials'
+import Footer from './components/Footer'
+
+
+const App = () => {
+  return (
+    <>
+      <Navbar/>
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <HeroSection/>
+        <FeatureSection/>
+        <WorkFlow/>
+        <Pricing/>
+        <Testimonials/>
+        <Footer/>
+      </div>
+      
+    </>
+    
+  )
+}
+
+export default App
